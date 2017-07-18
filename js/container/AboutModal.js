dae821670c9b8bb7eafb9baaa23fbffdb99963a5
weihongwei <hwwei@thoughtworks.com>
2017-07-18 14:29:01 +0800

    修改权利声明页面

--- js/container/AboutModal.js
+++ js/container/AboutModal.js
@@ -3,22 +3,38 @@ import ReactNative from 'react-native';
 import { Navigation } from 'react-native-navigation';
 
 const {
-	View, Text, Button
+	View, Text, Button, StyleSheet
 } = ReactNative;
 
 class AboutModal extends React.PureComponent {
 	render() {
 		return (
 			<View>
-				<Text>
-					解释权归作者所有，所有接口使用www.u148.net提供的API，如果违反有意思吧的使用权，请联系yourwafer@gmail.com，本人会尽快修改
+				<Text style={styles.txt}>
+					解释权归作者所有，所有接口使用www.u148.net提供的API，仅做个人学习使用，如果违反有意思吧的使用权，请联系yourwafer@gmail.com，本人会尽快修改.
 				</Text>
-				<Button title='关闭' onPress={()=>{
+				<View style={styles.btn}>
+					<Button color={'white'} title='关闭' onPress={()=>{
 					Navigation.dismissModal({
             animationType: 'slide-down'
 					});}} />
+				</View>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	btn: {
+		backgroundColor: '#00bccd',
+		width: 100,
+		alignSelf: 'center',
+		borderRadius: 3,
+		marginTop: 20
+	},
+	txt: {
+		margin: 10
+	}
+});
+
 export default AboutModal;
