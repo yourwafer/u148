f08075ffe464ab764ee52b1fc149b46df89fd97f
weihongwei <hwwei@thoughtworks.com>
2017-07-19 16:36:26 +0800

    可编译android

--- android/app/src/main/java/com/u148/MainActivity.java
+++ android/app/src/main/java/com/u148/MainActivity.java
@@ -1,15 +1,6 @@
 package com.u148;
 
-import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
-
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "u148";
-    }
+public class MainActivity extends SplashActivity {
 }
